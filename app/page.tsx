3d4d6ca268d326820a82b7ae6465c28cd32c8874
lmangall <l.mangallon@gmail.com>
2024-09-28 17:37:48 +0200

    refactor: clean page.tsx to integrate alpha version

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import GameOne from "./components/GameOne";
-import ChatComponent from "./components/ChatComponent";
+// import GameOne from "./components/GameOne";
+// import ChatComponent from "./components/ChatComponent";
 // import GameTwo from "./components/GameTwo";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <ChatComponent />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <GameOne />
+          {/* <ChatComponent /> */}
+          {/* <GameOne /> */}
+          <Button variant="outline">Play</Button>
         </main>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <a
