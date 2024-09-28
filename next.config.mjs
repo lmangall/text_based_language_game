7ef95a18c08e54743aa06a3789baa4a2fa30fc7c
lmangall <l.mangallon@gmail.com>
2024-09-28 22:06:49 +0200

    fix: build: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Add alias for the '@' prefix
+    config.resolve.alias["@"] = path.resolve(__dirname);
+
+    return config;
+  },
+};
 
 export default nextConfig;
